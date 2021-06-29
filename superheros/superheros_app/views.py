68ba5203b793c0a99b50f9befac94b06109a5198
Andy Wade <andyandamandawade@hotmail.com>
2021-06-28 21:34:21 -0500

    completed section: Rendering a Template

--- superheros/superheros_app/views.py
+++ superheros/superheros_app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("hellow world")
+    return render(request, 'superheros_app/index.html')
