54d4c99afc82561d4631c62bb91eb5e4458c5d90
Andy Wade <andyandamandawade@hotmail.com>
2021-06-28 22:34:52 -0500

    completed section: querying in Django's database API

--- superheros/superheros_app/views.py
+++ superheros/superheros_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import superhero_app
 from django.http import HttpResponse
 # Create your views here.
 
 
 def index(request):
+    all_superheros = superhero_app.objects.all()
+    context = {
+        'all_superheros': all_superheros
+    }
     return render(request, 'superheros_app/index.html')
