2def8154c60afff12b3bf613fd41cc161ffc0f25
Andy Wade <andyandamandawade@hotmail.com>
2021-06-29 20:19:26 -0500

    completed what I could of the tutorial, with errors

--- superheros/superheros_app/urls.py
+++ superheros/superheros_app/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name = 'superheros_app'
 urlpatterns = [
     path('', views.index, name='index'),
-#    path('<int:superheros_app>/', views.detail, name='detail'),
+    path('<int:superheros_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create_new_superhero')
 ]
 
--- superheros/superheros_app/views.py
+++ superheros/superheros_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import superhero_app
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.http import HttpResponse
 # Create your views here.
 
@@ -11,3 +13,20 @@ def index(request):
     }
     return render(request, 'superheros_app/index.html', context)
 
+
+def detail(request, superheros_id):
+    pass
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.Post.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_superhero_ability = request.POST.get('primary_superhero_ability')
+        catchphrase = request.POST.get('catchphrase')
+        new_superhero = superhero_app(name=name, genre=alter_ego, runtime=primary_superhero_ability,
+                                      release_date=catchphrase)
+        new_superhero.save()
+        return HttpResponseRedirect(reverse('superheros:index'))
+    else:
+        return render(request, 'superheros/create.html')
