e030f16510f07adabbe600be7daceae2aebe1467
Andy Wade <carrollandywadejr@gmail.com>
2021-08-04 22:51:58 -0500

    completed a superhero update function

--- superheros/superheros_app/urls.py
+++ superheros/superheros_app/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'superheros_app'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:superheros_id>/', views.detail, name='detail'),
-    path('new/', views.create, name='create_new_superhero')
+    path('detail/<int:superheros_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create_new_superhero'),
+    path('update/<int:superheros_id>/', views.update, name='update'),
+    path('kryptonite/<int:superheros_id>', views.kryptonite, name='kryptonite')
 ]
 
--- superheros/superheros_app/views.py
+++ superheros/superheros_app/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from .models import superhero_app
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import (get_object_or_404,
+                              render,
+                              HttpResponseRedirect)
+from django.forms import ModelForm
 from django.http import HttpResponse
+from django.contrib import messages
+from django.shortcuts import redirect
 # Create your views here.
 
 
@@ -20,6 +26,23 @@ def detail(request, superheros_id):
         'single_superheros_app': single_superheros_app
     }
     return render(request, 'superheros_app/detail.html', context)
+
+
+def update(request, superheros_id):
+    single_superheros_app = superhero_app.objects.get(id=superheros_id)
+    context = {
+        'single_superheros_app': single_superheros_app
+    }
+    if request.method == 'POST':
+        single_superheros_app.name = request.POST.get('name')
+        single_superheros_app.alter_ego = request.POST.get('alter_ego')
+        single_superheros_app.primary_superhero_ability = request.POST.get('primary_superhero_ability')
+        single_superheros_app.secondary_superhero_ability = request.POST.get('secondary_superhero_ability')
+        single_superheros_app.catchphrase = request.POST.get('catchphrase')
+        single_superheros_app.save()
+        return HttpResponseRedirect(reverse('superheros_app:index'))
+    else:
+        return render(request, 'superheros_app/update.html', context)
 
 
 def create(request):
@@ -40,10 +63,7 @@ def create(request):
         return render(request, 'superheros_app/create.html')
 
 
-def update(request, superheros_id):
-    single_superheros_app = superhero_app.objects.get(id=superheros_id)
-    context = {
-        'single_superheros_app': single_superheros_app
-    }
-    return render(request, 'superheros_app/update.html', context)
+def kryptonite(request, superheros_id):
+    superhero_app.objects.filter(id=superheros_id).delete()
+    return HttpResponseRedirect(reverse('superheros_app:index'))
 
